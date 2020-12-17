64494c2bac402d9b640f92c9b00f7fdf94f7de30
fabianbormann <bormann.fabian@gmail.com>
2020-12-17 22:47:42 +0100

    adjust integration settings

--- clementine.js
+++ clementine.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 const bunyan = require('bunyan');
 const ip = require('ip');
 const level = require('level');
+const { Input } = require('antd');
 const db = level('./db', { valueEncoding: 'json' });
 
 const app = express();
@@ -15,17 +16,7 @@ const logger = bunyan.createLogger({name: 'clementine'});
 logger.level(env.ABOTKIT_CLEMENTINE_LOG_LEVEL || 'info');
 const port = env.ABOTKIT_CLEMENTINE_PORT || 3035;
 
-const revokeUrlWhitelisting = async (url) => {
-
-}
-
-const whitelistUrl = async (url) => {
-
-}
-
-const generateHtmlIntegrationLink = async (bot, uuid) => {
-  return 'https://abotkit.io/integration/website-integration?bot=' + bot;
-}
+const maeveUrl = env.ABOTKIT_MAEVE_URL || 'http://localhost:3000';
 
 app.get('/', (req, res) => {
   res.status(200).send('"You\'re new. Not much of a rind on you, I\'ll give you a discount." - Clementine Pennyfeather');
@@ -35,59 +26,59 @@ app.get('/alive', (req, res) => {
   res.status(200).end();
 });
 
+const generateHtmlIntegrationLink = async bot => {
+  return `<script type="text/javascript" src="${maeveUrl}/integration/website-integration/ressource?bot=${bot}"></script>`;
+}
+
 /*
   This settings config will be requested by maeve and delivered to dolores and then rendered into input fields
   and allows the user to enter some settings
 */
 app.get('/settings', (req, res) => {
-  const settings = [{
-    type: 'input',
-    placeholder: {
-      de: 'Benutzername',
-      en: 'Username'
-    },
-    name: 'username'
-  }, {
-    type: 'input',
-    placeholder: {
-      de: 'Passwort',
-      en: 'Password'
-    },
-    name: 'password'
-  }, {
-    type: 'button',
-    id: 'submit-settings',
-    action: 'submit',
-    text: {
-      de: 'Absenden',
-      en: 'Submit'
-    },
-    postActionSnackbar: {
-      de: 'Deine Einstellungen wurden erfolgreich gespeichert', 
-      en: 'Successfully stored your preferences'
+  db.get('whitelist', (error, value) => {
+    if (error) {
+      logger.error(error);
+      return res.status(500).json(error);
+    } else {
+      const settings = [{
+        type: 'dynamic-list',
+        name: 'whitelist',
+        entries: JSON.stringify(value)
+      }, {
+        type: 'button',
+        id: 'submit-settings',
+        action: 'submit',
+        text: {
+          de: 'Übernehmen',
+          en: 'Apply'
+        },
+        postActionSnackbar: {
+          de: 'Deine Einstellungen wurden erfolgreich gespeichert', 
+          en: 'Successfully stored your preferences'
+        }
+      }, {
+        type: 'button',
+        id: 'generateLink',
+        action: 'execute',
+        postAction: (response => {
+          alert(response.data);
+        }).toString(),
+        text: {
+          de: 'Hallo Server',
+          en: 'Hello Server'
+        }
+      }, {
+        type: 'input',
+        placeholder: {
+          de: 'Script Element',
+          en: 'Script Element'
+        },
+        attributes: ['readonly', 'add_to_clipboard'],
+        value: generateHtmlIntegrationLink()
+      }];
+      res.json(settings);
     }
-  }, {
-    type: 'button',
-    id: 'hello-client',
-    action: (() => alert("hello")).toString(),
-    text: {
-      de: 'Hallo',
-      en: 'Hello'
-    }
-  }, {
-    type: 'button',
-    id: 'hello-server',
-    action: 'execute',
-    postAction: (response => {
-      alert(response.data);
-    }).toString(),
-    text: {
-      de: 'Hallo Server',
-      en: 'Hello Server'
-    }
-  }];
-
-  res.json(settings);
+  });
 })
 
 app.post('/settings', async (req, res) => {
@@ -108,34 +99,11 @@ app.post('/settings', async (req, res) => {
   Use this endpoint to return a static ressource
 */
 app.get('/ressouce', (req, res) => {
-  res.status(200).send('Any resssource.')
-})
-
-/*
-  You can add custom code if anyone needs to communicate with your integration via maeve
-*/
-app.post('/execute', (req, res) => {
-  db.get('settings', (error, value) => {
-    if (error) {
-      logger.error(error);
-      return res.status(500).json(error);
-    } else {
-      logger.info(value);
-
-      if (typeof value.username === 'undefined') {
-        res.status(200).send('hello');
-      } else {
-        res.status(200).send(`hello ${value.username}`);
-      }
-    }
-  });
+  res.status(200).send('console.log("hello world")');
 });
 
 app.listen(port, async () => {
   logger.info(`"I'm listening on port ${port}!`);
-
-  const maeveUrl = env.ABOTKIT_MAEVE_URL || 'http://localhost';
-  const maevePort = env.ABOTKIT_MAEVE_PORT || 3000;
 
   try {
     await axios.post(`${maeveUrl}:${maevePort}/integration`, {
@@ -155,12 +123,11 @@ app.listen(port, async () => {
   }
 
   try {
-    const settings = await db.get('settings');
-    console.log(settings);
+    await db.get('whitelist');
   } catch (error) {
     if (error.type === 'NotFoundError') {
-      logger.info('settings does not exist. Add empty object as value instead.')
-      await db.put('settings', {});
+      logger.info('whitelist does not exist. Add empty object as value instead.')
+      await db.put('whitelist', []);
     } else {
       logger.error(error);
     }
